6a2b583ac3d036554640b3a6eff58123e33a8a83
Faizan <faizan.m.patel10@gmail.com>
2025-03-03 14:57:11 +0530

    assests

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+      <div className='flex justify-between items-center py-5 fornt-medium '>
+          
+    </div>
+  )
+}
+
+export default NavBar
