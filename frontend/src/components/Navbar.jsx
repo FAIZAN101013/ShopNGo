6d5d3d10895cae6ef996d1b1514edf8d70902b91
Faizan <faizan.m.patel10@gmail.com>
2025-03-03 15:13:53 +0530

    Navbar made in progress

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import { assets } from '../assets/assets'
+import { NavLink } from 'react-router-dom'
 const NavBar = () => {
   return (
       <div className='flex justify-between items-center py-5 fornt-medium '>
-          
+          <img src={assets.logo} className='w-36' alt="" />
+          <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+              <NavLink className='flex  flex-col items-center gap-1'> 
+                  <p>Home</p>   
+                  <hr className='w-2/4  border-b-2 border-black'/>
+                  
+              </NavLink>
+         
+          </ul>
     </div>
   )
 }
