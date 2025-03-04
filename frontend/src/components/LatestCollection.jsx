2ff83b0102df0ab7b11628b47bf719eeb0c10456
Faizan <faizan.m.patel10@gmail.com>
2025-03-04 15:45:59 +0530

    add products

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,8 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
+import ProductItem from './productItem';
 
 function LatestCollection() {
+
+    const [latestProduct, setLatestProduct] = useState([])
+    
+    useEffect(() => {
+        setLatestProduct(products.slice(0,10 ))
+    },[])
     const { products } = useContext(ShopContext);
 
     return (
@@ -11,9 +18,14 @@ function LatestCollection() {
                 <Title text1={'LATEST'} text2={'COLLECTION'} />
                 <p className="max-w-lg text-xs sm:text-sm md:text-base text-gray-600 mt-2">
                     Step into the future with ShopNGo’s latest collection,  
-                    Where every ride is a seamless blend of style and perfection.  
-                    
+                    Where every ride is a seamless blend of style and perfection.     
                 </p>
+            </div>
+            {/**Rendering products */}
+            <div className='grid gird-col-2 sm:grid-cols-3 mg:grid-col-4 lg-grid-col-5 gap-y-6 pt-8 pb-10'>
+                {latestProduct.map((item, index) => (
+                    <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+                ))}
             </div>
         </div>
     );
---
+++ frontend/src/components/ProductItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { ShopContext } from '../context/ShopContext';
+import { useContext } from 'react';
+
+const ProductItem = ({ id, image, name, price }) => {
+    const {currency} = useContext(ShopContext);
+  return (
+      <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
+          <div className='overflow-hidden'>
+              <img  className='hover:scale-110 transition  ease-in-out' src={image[0]} alt="" />
+          </div>
+          <p className=' text-gray-900 font-medium text-lg truncate'>{name}</p>
+          <p className='text-gray-600 text-md mt-1'>{currency}{ price}</p>
+      </Link>
+     
+  )
+}
+
+export default ProductItem
