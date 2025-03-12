110ae08e88c2e9916c150454b9c763df078fe45c
Faizan <faizan.m.patel10@gmail.com>
2025-03-12 17:29:50 +0530

    Worked on search bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { assets } from '../assets/assets';
 import { Link, NavLink } from 'react-router-dom';
+import { ShopContext } from '../context/ShopContext';
 
 const NavBar = () => {
   const [visible, setVisible] = useState(false);
-
+  const {setShowSearch} = useContext(ShopContext);
   return (
     <div className="flex justify-between items-center py-5 font-medium px-5 pr-8 relative bg-white">
       
@@ -35,7 +36,7 @@ const NavBar = () => {
 
       {/* Icons Section */}
       <div className="flex items-center gap-6">
-        <img src={assets.search_icon} className="w-5 cursor-pointer hover:scale-110 transition" alt="Search Icon" />
+        <img onClick={()=>setShowSearch(true) } src={assets.search_icon} className="w-5 cursor-pointer hover:scale-110 transition" alt="Search Icon" />
 
         {/* Profile Dropdown */}
         <div className="relative group">
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,12 +6,13 @@ import Title from '../components/Title';
 import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filteredProducts, setFilteredProducts] = useState(products);
 
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState('relevant');
 
   const toggleCategory = (e) => {
     if (category.includes(e.target.value)) {
@@ -31,6 +32,12 @@ const Collection = () => {
 
   const applyFilter = useCallback(() => {
     let productsCopy = products.slice();
+    console.log('applyFilter called');
+    console.log('showSearch:', showSearch, 'search:', search);
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
+    }
+
     if (category.length > 0) {
       productsCopy = productsCopy.filter((item) => category.includes(item.category));
     }
@@ -38,30 +45,43 @@ const Collection = () => {
       productsCopy = productsCopy.filter((item) => subCategory.includes(item.subCategory));
     }
     setFilteredProducts(productsCopy);
-  }, [products, category , subCategory]);
+  }, [products, category, subCategory, search, showSearch]);
 
-
-
-  useEffect(() => {
-    setFilteredProducts(products);
-  }, [products]);
+  const sortProducts = () => {
+    let fpCopy = filteredProducts.slice();
+    switch (sortType) {
+      case 'low-high':
+        setFilteredProducts(fpCopy.sort((a, b) => a.price - b.price));
+        break;
+      case 'high-low':
+        setFilteredProducts(fpCopy.sort((a, b) => b.price - a.price));
+        break;
+      default:
+        applyFilter();
+        break;
+    }
+  };
 
   useEffect(() => {
     applyFilter();
-  }, [category, subCategory, applyFilter]);
+  }, [category, subCategory, applyFilter, search, showSearch]);
+
+  useEffect(() => {
+    sortProducts();
+  }, [sortType]);
 
   return (
-    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 !pt-10'>
+    <div className='flex flex-col sm:flex-row !gap-1 sm:gap-10 !pt-10'>
       {/** filter option */}
       <div className='min-w-60'>
-        <p onClick={() => setShowFilter(!showFilter)} className='my-4 text-xl flex items-center cursor-pointer gap-2'>
+        <p onClick={() => setShowFilter(!showFilter)} className='!my-4 text-xl flex items-center cursor-pointer gap-2'>
           FILTERS
           <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} src={assets.dropdown_icon} alt='Dropdown Icon' />
         </p>
 
         {/** category filter */}
         <div className={`border border-gray-300 !pl-5 !py-3 !my-5 ${showFilter ? '' : 'hidden'} sm:block`}>
-          <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+          <p className='!mb-3 text-sm font-medium'>CATEGORIES</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex !gap-2'>
               <input className='w-3' type='checkbox' value={'Men'} onChange={toggleCategory} /> Men
@@ -93,18 +113,18 @@ const Collection = () => {
       </div>
 
       {/** right side */}
-      <div className='flex-1'>
-        <div className='flex justify-between text-base !mb-4'>
+      <div className='flex-1 !ml-10'>
+        <div className='flex justify-between text-base !mb-4 !pt-6'>
           <Title text1={'ALL'} text2={'COLLECTIONS'} />
           {/** sort by */}
-          <select className='border-2 border-gray-300 text-sm px-2'>
+          <select onChange={(e) => setSortType(e.target.value)} className='border-2 border-gray-300 text-sm !px-2'>
             <option value='relevant'>Sort by: Relevant</option>
             <option value='low-high'>Sort by: Low to High</option>
             <option value='high-low'>Sort by: High to Low</option>
           </select>
         </div>
         {/** product */}
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6'>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 !gap-6'>
           {filteredProducts.map((item, index) => (
             <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
           ))}
